67d99cfd0439689cd8f7ae583433d0eb6e5bd72c
krulex <krulex98@gmail.com>
2020-07-19 23:47:26 +0300

    photoshop: task 2.1

---
+++ _Photoshop/Filters/GrayscaleFilter.cs
@@ -0,0 +1,24 @@
+﻿namespace MyPhotoshop
+{
+	public class GrayscaleFilter: IFilter
+	{
+		public ParameterInfo[] GetParameters() =>  new ParameterInfo[]{};
+
+		public override string ToString() => "Черно/Белый";
+
+		public Photo Process(Photo original, double[] parameters)
+		{
+			var result = new Photo(original.Width, original.Height);
+			
+			for (var x = 0; x < result.Width; x++)
+				for (var y = 0; y < result.Height; y++)
+				{
+					var lightness = original[x, y].R + original[x, y].B + original[x, y].G;
+					lightness /= 3;
+					result[x, y] = new Pixel(lightness, lightness, lightness);
+				} 
+
+			return result;
+		}
+	}
+}
--- _Photoshop/Main.cs
+++ _Photoshop/Main.cs
@@ -10,6 +10,7 @@ namespace MyPhotoshop
 		{
 			var window = new MainWindow();
 			window.AddFilter(new LighteningFilter());
+			window.AddFilter(new GrayscaleFilter());
 			Application.Run(window);
 		}
 	}
