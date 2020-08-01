ca9499b43edfab5f8a56e415a33c349ada078487
krulex <krulex98@gmail.com>
2020-08-01 23:08:33 +0300

    5.3 remove useless usings

--- 5.3/Inheritance.MapObjects/Army.cs
+++ 5.3/Inheritance.MapObjects/Army.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Inheritance.MapObjects
+﻿namespace Inheritance.MapObjects
 {
     public class Army
     {
