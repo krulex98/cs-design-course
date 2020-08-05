4e5ba26e292a7d36668005dfa42bd7a24cbe3f54
krulex <krulex98@gmail.com>
2020-08-05 22:17:18 +0300

    5.3 third task completed

--- 5.3/Inheritance.Geometry/Task.cs
+++ 5.3/Inheritance.Geometry/Task.cs
@@ -1,43 +1,41 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Inheritance.Geometry
 {
-	public class Body
+	public abstract class Body
 	{
-		public double GetVolume()
-		{
-			if (this is Ball)
-				return 4.0 * Math.PI * Math.Pow(((Ball) this).Radius, 3) / 3;
-			if (this is Cube)
-				return Math.Pow(((Cube) this).Size, 3);
-			if (this is Cylinder)
-			{
-				var c = this as Cylinder;
-				return Math.PI * Math.Pow(c.Radius, 2) * c.Height;
-			}
-
-			throw new Exception();
-		}
+		public abstract double GetVolume();
 	}
 
 	public class Ball : Body
 	{
 		public double Radius { get; set; }
+
+		public override double GetVolume()
+		{
+			return 4.0 * Math.PI * Math.Pow(Radius, 3) / 3;
+		}
 	}
 
 	public class Cube : Body
 	{
 		public double Size { get; set; }
+		
+		public override double GetVolume()
+		{
+			return Math.Pow(Size, 3);
+		}
 	}
 
 	public class Cylinder : Body
 	{
 		public double Height { get; set; }
 		public double Radius { get; set; }
+		
+		public override double GetVolume()
+		{
+			return Math.PI * Math.Pow(Radius, 2) * Height;
+		}
 	}
 
 	// Заготовка класса для задачи на Visitor
