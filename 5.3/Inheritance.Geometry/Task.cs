3e91402f7bc911f0e9dad3e76cf946da54acc9cd
krulex <krulex98@gmail.com>
2020-08-05 23:03:42 +0300

    5.3 fourth task completed

--- 5.3/Inheritance.Geometry/Task.cs
+++ 5.3/Inheritance.Geometry/Task.cs
@@ -5,11 +5,18 @@ namespace Inheritance.Geometry
 	public abstract class Body
 	{
 		public abstract double GetVolume();
+
+		public abstract void Accept(IVisitor visitor);
 	}
 
 	public class Ball : Body
 	{
 		public double Radius { get; set; }
+
+		public override void Accept(IVisitor visitor)
+		{
+			visitor.Visit(this);
+		}
 
 		public override double GetVolume()
 		{
@@ -20,6 +27,11 @@ namespace Inheritance.Geometry
 	public class Cube : Body
 	{
 		public double Size { get; set; }
+		
+		public override void Accept(IVisitor visitor)
+		{
+			visitor.Visit(this);
+		}
 		
 		public override double GetVolume()
 		{
@@ -32,20 +44,62 @@ namespace Inheritance.Geometry
 		public double Height { get; set; }
 		public double Radius { get; set; }
 		
+		public override void Accept(IVisitor visitor)
+		{
+			visitor.Visit(this);
+		}
+		
 		public override double GetVolume()
 		{
 			return Math.PI * Math.Pow(Radius, 2) * Height;
 		}
 	}
 
-	// Заготовка класса для задачи на Visitor
-	public class SurfaceAreaVisitor
+	public interface IVisitor
+	{
+		void Visit(Ball ball);
+		void Visit(Cube cube);
+		void Visit(Cylinder cylinder);
+	}
+	
+	public class SurfaceAreaVisitor: IVisitor
 	{
 		public double SurfaceArea { get; private set; }
+
+		public void Visit(Ball ball)
+		{
+			SurfaceArea = 4 * Math.PI * ball.Radius * ball.Radius;
+		}
+
+		public void Visit(Cube cube)
+		{
+			SurfaceArea = 6 * cube.Size * cube.Size;
+		}
+
+		public void Visit(Cylinder cylinder)
+		{
+			SurfaceArea = 2 * Math.PI * cylinder.Radius * (cylinder.Radius + cylinder.Height);
+		}
 	}
 
-	public class DimensionsVisitor
+	public class DimensionsVisitor: IVisitor
 	{
 		public Dimensions Dimensions { get; private set; }
+
+		public void Visit(Ball ball)
+		{
+			var doubleR = ball.Radius * 2;
+			Dimensions = new Dimensions(doubleR, doubleR);
+		}
+
+		public void Visit(Cube cube)
+		{
+			Dimensions = new Dimensions(cube.Size, cube.Size);
+		}
+
+		public void Visit(Cylinder cylinder)
+		{
+			Dimensions = new Dimensions(cylinder.Radius * 2, cylinder.Height);
+		}
 	}
 }
