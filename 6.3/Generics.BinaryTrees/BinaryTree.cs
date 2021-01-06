5706d2001f9623e3877b20aeb1ebb792a89d9c78
krulex <krulex98@gmail.com>
2021-01-06 13:00:50 +0300

    6.3 first task completed

--- 6.3/Generics.BinaryTrees/BinaryTree.cs
+++ 6.3/Generics.BinaryTrees/BinaryTree.cs
@@ -1,13 +1,111 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generics.BinaryTrees
 {
-    public class BinaryTree
-    {
-        
-    }
+	public class Node<T>
+		where T: IComparable<T>
+	{
+		public T Value { get; }
+		
+		public Node<T> Left { get; set; }
+		public Node<T> Right { get; set; }
+
+		public Node(T value)
+		{
+			Value = value;
+			Left = Right = null;
+		}
+	}
+
+	public class BinaryTree<T> : IEnumerable<T>
+		where T: IComparable<T>
+	{
+		private Node<T> _root;
+
+		public T Value => _root.Value;
+		public Node<T> Left => _root.Left;
+		public Node<T> Right => _root.Right;
+
+		public BinaryTree()
+		{
+			_root = null;
+		}
+		
+		public void Add(T item)
+		{
+			if (_root == null)
+			{
+				_root = new Node<T>(item);
+				return;
+			}
+			
+			AddInner(new Node<T>(item));
+		}
+
+		private void AddInner(Node<T> newNode)
+		{
+			var curr = _root;
+			Node<T> nodeForInsert = null;
+
+			while (curr != null)
+			{
+				nodeForInsert = curr;
+
+				if (newNode.Value.CompareTo(curr.Value) <= 0)
+					curr = curr.Left;
+				else
+					curr = curr.Right;
+			}
+
+			if (newNode.Value.CompareTo(nodeForInsert.Value) <= 0)
+				nodeForInsert.Left = newNode;
+			else
+				nodeForInsert.Right = newNode;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			if (_root == null)
+				yield break;
+
+			var curr = _root;
+			var stack = new Stack<Node<T>>();
+
+			while (stack.Count > 0 || curr != null)
+			{
+				while (curr != null)
+				{
+					stack.Push(curr);
+					curr = curr.Left;
+				}
+
+				curr = stack.Pop();
+
+				yield return curr.Value;
+
+				curr = curr.Right;
+			}
+		}
+		
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+
+	public static class BinaryTree
+	{
+		public static IEnumerable<int> Create(params int[] arr)
+		{
+			var tree = new BinaryTree<int>();
+			foreach (var item in arr)
+			{
+				tree.Add(item);
+			}
+
+			return tree;
+		}
+	}
 }
--- 6.3/Generics.BinaryTrees/BinaryTree_should.cs
+++ 6.3/Generics.BinaryTrees/BinaryTree_should.cs
@@ -1,8 +1,6 @@
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Generics.BinaryTrees
