a9d5b282043d04443cf88c0fea62b2b0d99060d1
krulex <krulex98@gmail.com>
2021-01-06 13:14:21 +0300

    6.3 second task init

---
+++ 6.3/Generics.Tables/Table.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Generics.Tables
+{
+	public class Table<TRow, TColumn, TCell>
+	{
+		public Table()
+		{
+			
+		}
+
+		public void AddRow(TRow row)
+		{
+			
+		}
+
+		public void AddColumn(TColumn column)
+		{
+			
+		}
+	}
+}
