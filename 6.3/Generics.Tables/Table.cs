e5b55db84043e231a1f70577a93c3df922e5b08b
krulex <krulex98@gmail.com>
2021-01-09 21:32:56 +0300

    6.3 second task improvement

--- 6.3/Generics.Tables/Table.cs
+++ 6.3/Generics.Tables/Table.cs
@@ -6,120 +6,92 @@ namespace Generics.Tables
 {
 	public class Table<TRow, TColumn, TCell>
 	{
-		private readonly IndexedTable<TRow, TColumn, TCell> _table;
-		
-		public Table()
-		{
-			_table = new IndexedTable<TRow, TColumn, TCell>();
-		}
-		
-		public IndexedTable<TRow, TColumn, TCell> Open
-		{
-			get => _table;
-		}
+		private readonly List<TRow> _rows = new List<TRow>();
+		private readonly List<TColumn> _columns = new List<TColumn>();
+		private readonly List<List<TCell>> _matrix = new List<List<TCell>>();
 
-		public IndexedTable<TRow, TColumn, TCell> Existed
-		{
-			get => new IndexedTable<TRow, TColumn, TCell>(_table, true);
-		}
+		public List<TRow> Rows => _rows;
+		public List<TColumn> Columns => _columns;
 
-		public void AddRow(TRow row) => _table.AddRow(row);
-		public void AddColumn(TColumn column) => _table.AddColumn(column);
+		public IndexedTable Open => new IndexedTable(this, false);
 
-		public IEnumerable<TRow> Rows => _table.Rows;
-		public IEnumerable<TColumn> Columns => _table.Columns;
-	}
-
-	public class IndexedTable<TRow, TColumn, TCell> 
-	{
-		public List<TRow> Rows { get; }
-		public List<TColumn> Columns { get; }
-
-		private List<List<TCell>> _table;
-		
-		public bool WithArgumentCheck { get; set; }
-
-		public IndexedTable()
-		{
-			Rows = new List<TRow>();
-			Columns = new List<TColumn>();
-			_table = new List<List<TCell>>();
-		}
-
-		public IndexedTable(IndexedTable<TRow, TColumn, TCell> table, bool withArgumentCheck)
-		{
-			Rows = new List<TRow>(table.Rows);
-			Columns = new List<TColumn>(table.Columns);
-			_table = new List<List<TCell>>(table._table);
-
-			WithArgumentCheck = withArgumentCheck;
-		}
-
-		public TCell this[TRow row, TColumn column]
-		{
-			get
-			{
-				var indexRow = Rows.FindIndex(r => r.Equals(row));
-				var indexCol = Columns.FindIndex(c => c.Equals(column));
-
-				if (indexRow < 0 || indexCol < 0)
-				{
-					if (WithArgumentCheck)
-						throw new ArgumentException();
-					
-					return default;
-				}
-
-				return _table[indexRow][indexCol];
-			}
-			set
-			{
-				var rowExists = Rows.Contains(row);
-				var colExists = Columns.Contains(column);
-
-				if (!rowExists || !colExists)
-				{
-					if (WithArgumentCheck)
-					{
-						throw new ArgumentException();
-					}
-					
-					if (!rowExists)
-						AddRow(row);
-						
-					if (!colExists)
-						AddColumn(column);
-				}
-
-				var indexRow = Rows.FindIndex(r => r.Equals(row));
-				var indexCol = Columns.FindIndex(c => c.Equals(column));
-				
-				if (indexRow < 0 || indexCol < 0)
-					throw new ArgumentException();
-
-				_table[indexRow][indexCol] = value;
-			}
-		}
+		public IndexedTable Existed => new IndexedTable(this, true);
 
 		public void AddRow(TRow row)
 		{
-			if (!Rows.Contains(row))
+			if (!_rows.Contains(row))
 			{
-				Rows.Add(row);
-
-				var cols = Columns.Select(col => (TCell) default).ToList();
-				_table.Add(cols);
+				_rows.Add(row);
+				_matrix.Add(_columns.Select(col => (TCell) default).ToList());
 			}
 		}
 
 		public void AddColumn(TColumn col)
 		{
-			if (!Columns.Contains(col))
+			if (!_columns.Contains(col))
 			{
-				Columns.Add(col);
+				_columns.Add(col);
 
-				foreach (var row in _table)
+				foreach (var row in _matrix)
 					row.Add(default);
+			}
+		}
+
+		public class IndexedTable
+		{
+			private readonly Table<TRow, TColumn, TCell> _table;
+			private readonly bool _withArgumentCheck;
+
+			public IndexedTable(Table<TRow, TColumn, TCell> table, bool withArgumentCheck)
+			{
+				_table = table;
+				_withArgumentCheck = withArgumentCheck;
+			}
+
+			public TCell this[TRow row, TColumn column]
+			{
+				get
+				{
+					var indexRow = _table.Rows.FindIndex(r => r.Equals(row));
+					var indexCol = _table.Columns.FindIndex(c => c.Equals(column));
+
+					if (indexRow < 0 || indexCol < 0)
+					{
+						if (_withArgumentCheck)
+							throw new ArgumentException();
+
+						return default;
+					}
+
+					return _table._matrix[indexRow][indexCol];
+				}
+				set
+				{
+					var rowExists = _table.Rows.Contains(row);
+					var colExists = _table.Columns.Contains(column);
+
+					if (!rowExists || !colExists)
+					{
+						if (_withArgumentCheck)
+						{
+							throw new ArgumentException();
+						}
+
+						if (!rowExists)
+							_table.AddRow(row);
+
+						if (!colExists)
+							_table.AddColumn(column);
+					}
+
+					var indexRow = _table.Rows.FindIndex(r => r.Equals(row));
+					var indexCol = _table.Columns.FindIndex(c => c.Equals(column));
+
+					if (indexRow < 0 || indexCol < 0)
+						throw new ArgumentException();
+
+					_table._matrix[indexRow][indexCol] = value;
+				}
 			}
 		}
 	}
